a79ac1c546e5c3544cb089f493bffc9023e71d46
ghostayee <mayeeayee@outlook.com>
2025-11-03 10:59:21 +0300

    update another possibility

--- task3.py
+++ task3.py
@@ -1,6 +1,8 @@
 phone_number = input("Enter your phone number: ")
 if phone_number.startswith("07") and len(phone_number) == 10:
     print(f"+254{phone_number[1:]}")
+elif phone_number.startswith("7") and len(phone_number) == 9:
+    print(f"+254{phone_number}")
 elif phone_number.startswith("254") and len(phone_number) == 12:
     print(f"+{phone_number}")
 elif phone_number.startswith("01") and len(phone_number) == 10:
