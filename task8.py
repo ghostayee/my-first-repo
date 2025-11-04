b8ea4063b5d18dd63fcac21c62b35d12b9dd628a
ghostayee <mayeeayee@outlook.com>
2025-11-04 12:22:32 +0300

    updated task 8

--- task8.py
+++ task8.py
@@ -6,11 +6,17 @@
 # should print: “License suspended”.
 
 car_speed = float(input("Enter speed of the car:"))
+speed_limit = 70
+
 if car_speed < 70:
     print("Ok")
 else:
-    points = (car_speed - 70) // 5
-    if points > 12:
+    speed_exceeded = car_speed - speed_limit
+    demerits_points = (car_speed - speed_limit) // 5
+    if speed_exceeded % 5 != 0:
+        demerits_points += 1
+    if demerits_points > 12:
+        print(f"demerits_points- {demerits_points}")
         print("license suspended")
     else:
-        print("points =",  points)
+        print(f"points = {demerits_points}")
