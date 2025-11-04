5598e9698f7d44099784c79d9b2f081d2fd0bc2d
ghostayee <mayeeayee@outlook.com>
2025-11-04 12:49:46 +0300

    format numbers

--- task3.py
+++ task3.py
@@ -1,3 +1,11 @@
+# Write a program which gets a phone number from a form input or terminal.
+# Validates the phone number by checking if it starts with
+# +254.. or 07.. or 7… or 254.. or 01... or  1.. Convert the number
+#  to start with +254…
+# e.g if a user enters “0712345678”, the program should display “+254712345678”
+# e.g if a user enters “0112345678”, the program should display “+254112345678”
+# e.g if a user enters “712345678”, the program should display “+254712345678”
+
 phone_number = input("Enter your phone number: ")
 if phone_number.startswith("07") and len(phone_number) == 10:
     print(f"+254{phone_number[1:]}")
