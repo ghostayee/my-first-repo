368f2a7c5e8e669222eb9f333d8898ed6ab86a58
ghostayee <mayeeayee@outlook.com>
2025-11-05 08:56:08 +0300

    task 14 clearnce two numbers takena inputs

--- task14.py
+++ task14.py
@@ -1,7 +1,7 @@
 # TASK 14
 # Write a program that takes input of 2 values and adds them.
-# The program should only accept numbers and floats only or 
-# otherwise display an error “invalid character entered” and 
+# The program should only accept numbers and floats only or
+# otherwise display an error “invalid character entered” and
 # take the user to re-enter the inputs .
 
 valid = False
